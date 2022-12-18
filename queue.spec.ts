4f93ba33f37214e4d2996d132f9bca5cdd1432f2
Evgeny Artemov <eartemov013@gmail.com>
2022-12-18 16:48:54 +0400

    add tests for queue

---
+++ queue.spec.ts
@@ -0,0 +1,33 @@
+import Queue from './Queue';
+import { describe, expect, test } from '@jest/globals';
+test('queue', function () {
+  const queue = new Queue<number>();
+
+  queue.enqueue(5);
+  queue.enqueue(7);
+  queue.enqueue(9);
+
+  expect(queue.dequeue()).toEqual(5);
+  expect(queue.length).toEqual(2);
+
+  // this must be wrong..?
+  // debugger;
+
+  // i hate using debuggers
+  queue.enqueue(11);
+  expect(queue.dequeue()).toEqual(7);
+  expect(queue.dequeue()).toEqual(9);
+  expect(queue.peek()).toEqual(11);
+  expect(queue.dequeue()).toEqual(11);
+  expect(queue.dequeue()).toEqual(undefined);
+  expect(queue.length).toEqual(0);
+
+  // just wanted to make sure that I could not blow up myself when i remove
+  // everything
+  console.log('queue before:', queue);
+  queue.enqueue(69);
+  console.log('queue after:', queue);
+
+  expect(queue.peek()).toEqual(69);
+  expect(queue.length).toEqual(1);
+});
