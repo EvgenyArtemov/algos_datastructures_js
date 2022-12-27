fd8a9ba56c0c1875b40d157fe868cef2cbe21ba7
Evgeny Artemov <eartemov013@gmail.com>
2022-12-27 21:44:17 +0400

    fix binary search

--- binarySearch.ts
+++ binarySearch.ts
@@ -1,16 +1,15 @@
-const arr = [1, 2, 3, 4];
-
 export const binarySearch = (arr, target) => {
   let left = 0;
-  let right = arr.length - 1;
+  let right = arr.length;
 
   while (left < right) {
     const middle = Math.floor((right - left) / 2 + left);
     if (arr[middle] === target) return true;
     if (arr[middle] < target) {
-      right = middle - 1;
+      left = middle + 1;
     } else {
-      left = middle;
+      right = middle;
     }
   }
+  return false;
 };
